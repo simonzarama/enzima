316cedcd0e88501803e3c08a3a39d6dd5d6abb00
simonzarama <simonzarama@gmail.com>
2023-11-15 16:37:52 -0500

    Agregado STATIC_ROOT a settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zn!!^(#4=&#0qlya!20$yw__+sg#u2y&d0ap$f=$n#+058ai9%'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '.vercel.appp']
 
@@ -148,6 +149,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'myapp', 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # MEDIA
